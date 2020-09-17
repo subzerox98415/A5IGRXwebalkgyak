c4547d48e8202c55f72acf448352894af4e90c3b
Nyilas Viktor Balázs <vikidekingi@gmail.com>
2020-09-17 11:25:34 +0200

    sum

---
+++ firstgyak/src/firstgyak/Gyal.java
@@ -0,0 +1,20 @@
+package firstgyak;
+
+public class Gyal {
+
+	public static void main(String[] args) {
+		int result =sum();
+		System.out.println(result);
+
+	}
+	public static int sum ()
+	{
+		int num1 =1;
+		int num2 =2;
+		int num3 =3;
+		return num1+num2+num3;
+		
+		
+	}
+
+}
